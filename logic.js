3d98ef6dbe848527eca56c2c223b0b7ff5d2e1fb
esdalee <es.da.lee@gmail.com>
2019-06-04 20:23:58 -0400

    display recipe list

--- logic.js
+++ logic.js
@@ -6,13 +6,13 @@
 var baseQuery = "https://api.edamam.com/search?q=";
 var apiKey = "f43e58c104b981cd9a7ef77393c1cbad";
 var appId = "a2545d79";
-var ingrSearch = "chicken";
+var ingrSearch = "";
 
 
 // Click handler for ingredients submit button
 
-$(".btn").on("click", function(event) {
-
+$(".btn").on("click", function(e) {
+e.preventDefault();
 // Construct new query string with user inputs
 var newURL = baseQuery + ingrSearch + "&app_id=" + appId + "&app_key=" + apiKey;
 
@@ -29,7 +29,17 @@ $.ajax({
     for (var i = 0; i < numRecipes; i++) {
         var recipe = response.hits[i].recipe.url
         console.log(recipe); 
+
+        var recipeDiv = $("<div>");
+        recipeDiv.text(recipe);
+        // Adding the button to the HTML
+        $("#recipeList").append(recipeDiv);
     }
+
+
+
+
+
 });
 
 });
